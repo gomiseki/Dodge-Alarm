7beffa2d1e643221b9e8f3fbadabae615fadf610
Juhyeon <gomi.dev1755@gmail.com>
2022-09-20 12:18:04 +0900

    [add] selectbox component

--- process_renderer/components/selectBox.tsx
+++ process_renderer/components/selectBox.tsx
@@ -1,3 +1,4 @@
+import { darken, lighten } from 'polished';
 import React, {
   Dispatch, SetStateAction, useRef, RefObject, useEffect, useState,
 } from 'react';
@@ -31,30 +32,26 @@ const Label = styled.label<{ width: number | string, type: string }>`
   position: relative;
   display: inline-block;
   width: ${({ width }) => (typeof width === 'string' ? width : `${width}px`)};
-  color: ${({ theme }) => theme.palette.gray};
-  margin: ${({ type }) => (type === 'register' ? '10px 0px' : '0')};
 `;
 
 const Select = styled.select<{type: string}>`
-  padding: ${({ type }) => (type === 'register' ? '0rem' : '1rem')};
+  padding: 4px 5px;
   width: 100%;
   outline: none;
   border: none;
-  color: ${({ theme }) => theme.palette.gray};
-  border-bottom: 1px solid ${({ theme }) => theme.palette.borderGray};
+  color: ${({ theme }) => theme.palette.string};
   border-radius: 4px;
   cursor: pointer;
-  font-size: ${({ type }) => (type === 'register' ? '1.5rem' : '2rem')};
+  font-size: 18px;
 `;
 
 const SelectItemContainer = styled.div<{type: string}>`
   margin-top: 0.5rem;
-  border: 1px solid ${({ theme }) => theme.palette.borderGray};
   border-radius: 5px;
   box-shadow: 0px 4px 13px rgba(0, 0, 0, 0.25);
-  position: ${({ type }) => (type === 'register' ? 'absolute' : null)};
-  width: ${({ type }) => (type === 'register' ? '100%' : null)};
-  background-color: ${({ type }) => (type === 'register' ? 'white' : null)};
+  position: absolute;
+  width: 100%;
+  background-color: white;
   z-index: 1;
 
   -webkit-animation: 0.3s linear normal slide_down;
@@ -70,40 +67,44 @@ const SelectItemContainer = styled.div<{type: string}>`
   }
 `;
 
-const SelectItem = styled.option<{type:string}>`
-  height: ${({ type }) => (type === 'register' ? '1.5rem' : '2.5rem')};
-  font-size: ${({ type }) => (type === 'register' ? '1.5rem' : '2rem')};
+const SelectItem = styled.option<{editionId?:string, type:string}>`
+  padding: 6px 10px;
+  height: 16px;
+  border-radius: 4px;
+  font-size:18px;
+  font-weight: 600;
   cursor: pointer;
-  border-bottom: 1px solid ${({ theme }) => theme.palette.borderGray};
-  padding: 1rem 1rem 2rem 1rem;
+  color: ${({ theme }) => theme.palette.string};
   :hover {
-    color: ${({ theme }) => theme.palette.eliceViolet}
+    color: white;
+    background-color: ${({ theme, editionId }) => lighten(0.1, theme.editions[editionId] ? theme.editions[editionId].themeMain : theme.palette.themeMain)};
   }
 `;
 
 const defaultProps = {
   requestFunc: null,
   type: '',
+  editionId: '',
 };
 
 type ISelectBoxProps = {
   options: string[] | number[];
   defaultValue: string;
   selectedOption: string | number;
-  setSelectedOption: Dispatch<SetStateAction<string>> | Dispatch<SetStateAction<number>>;
   width: string | number;
   requestFunc?: any;
   type?: string;
+  editionId?: string;
 } & typeof defaultProps;
 
 function SelectBox({
   options,
   defaultValue,
   selectedOption,
-  setSelectedOption,
   width,
   requestFunc,
   type,
+  editionId,
 }: ISelectBoxProps) {
   const labelRef = useRef<HTMLLabelElement>(null);
   const [clickSelectedBox, setClickSelectedBox] = useSelect(labelRef); // SelectBox가 클릭됐는지 여부 체크
@@ -111,19 +112,16 @@ function SelectBox({
   // SelectBox 클릭 이벤트 핸들러
   const handleOpenSelectBox = (e: React.MouseEvent) => {
     e.preventDefault();
-    if (e.button === 0) { setClickSelectedBox(true); }
+    if (e.button === 0) { setClickSelectedBox(!clickSelectedBox); }
   };
 
   // requestFunc로 넘어온 API 요청 등을 여기서 처리
   const handleSelectItemClick = (e: any) => {
     const optionValue = e.target.value; // 선택한 option
 
-    setSelectedOption(optionValue); // 선택된 Option Value 설정
+    // setSelectedOption(optionValue); // 선택된 Option Value 설정
     setClickSelectedBox(false); // Option 선택하면 Select Box 닫기
-
-    if (requestFunc) {
-      requestFunc(optionValue); // 선택된 Option을 인자로 요청 함수 실행
-    }
+    requestFunc(optionValue); // 선택된 Option을 인자로 요청 함수 실행
   };
 
   return (
@@ -145,6 +143,7 @@ function SelectBox({
         <SelectItemContainer type={type}>
           {options.map((option, i) => (
             <SelectItem
+              editionId={editionId}
               key={String(i) + option}
               onClick={handleSelectItemClick}
               type={type}
